737a25b1351cfd460291570709f159f3abfdd1a8
zhengp0 <zhengp@uw.edu>
2020-12-14 19:50:27 -0800

    add get time min and max function

--- src/emmodel/data.py
+++ src/emmodel/data.py
@@ -69,13 +69,26 @@ class DataProcessor:
             df = df[df[col].isin(vals)]
         return df.reset_index(drop=True)
 
+    def get_time_min(self, df) -> Tuple[int, int]:
+        year_min = df[self.col_year].min()
+        tunit_min = df[df[self.col_year] == year_min][self.col_tunit].min()
+        return (year_min, tunit_min)
+
+    def get_time_max(self, df) -> Tuple[int, int]:
+        year_max = df[self.col_year].max()
+        tunit_max = df[df[self.col_year] == year_max][self.col_tunit].max()
+        return (year_max, tunit_max)
+
     def process(self,
                 df: pd.DataFrame,
-                time_start: Tuple[int, int] = (2010, 0),
-                time_end: Tuple[int, int] = (2020, 8),
+                time_start: Tuple[int, int] = None,
+                time_end: Tuple[int, int] = None,
                 offset_id: int = 0,
                 offset_col: str = "population",
-                group_specs: Dict[str, List] = dict()) -> pd.DataFrame:
+                group_specs: Dict[str, List] = None) -> pd.DataFrame:
+        time_start = self.get_time_min(df) if time_start is None else time_start
+        time_end = self.get_time_max(df) if time_end is None else time_end
+        group_specs = dict() if group_specs is None else group_specs
         df = self.select_cols(df)
         df = self.rename_cols(df)
         df = self.add_time(df, time_start, time_end)
