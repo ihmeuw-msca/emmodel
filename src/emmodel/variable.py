f81856c512a61840c1ec1b64160facd531ae4028
zhengp0 <zhengp@uw.edu>
2020-12-17 21:22:49 -0800

    add linear model to variables

--- src/emmodel/variable.py
+++ src/emmodel/variable.py
@@ -7,19 +7,28 @@ import regmod
 
 
 class ModelVariables:
-    def __init__(self, variables: List[regmod.variable.Variable]):
+    def __init__(self,
+                 variables: List[regmod.variable.Variable],
+                 model_type: str = "Poisson"):
         self.variables = variables
         self.var_names = [variable.name for variable in self.variables]
         self.var_dict = {var_name: self.variables[i]
                          for i, var_name in enumerate(self.var_names)}
+        self.model_type = model_type
+        if self.model_type not in ["Poisson", "Linear"]:
+            raise ValueError("`model_type` has to be 'Poisson' or 'Linear'.")
 
     def add_priors(self, priors: Dict[str, regmod.prior.Prior]):
         for var_name, prior in priors.items():
             if var_name in self.var_names:
                 self.var_dict[var_name].add_priors(prior)
 
-    def get_model(self, data: regmod.data.Data) -> regmod.model.PoissonModel:
-        return regmod.model.PoissonModel(data, self.variables, use_offset=True)
+    def get_model(self, data: regmod.data.Data) -> regmod.model.Model:
+        if self.model_type == "Poisson":
+            model = regmod.model.PoissonModel(data, self.variables, use_offset=True)
+        else:
+            model = regmod.model.LinearModel(data, self.variables, use_offset=True)
+        return model
 
     def result_to_priors(self,
                          result: Dict[str, np.ndarray],
@@ -34,8 +43,8 @@ class ModelVariables:
 
 
 class SeasonalityModelVariables(ModelVariables):
-    def __init__(self, variables: List[regmod.variable.Variable]):
-        super().__init__(variables)
+    def __init__(self, variables: List[regmod.variable.Variable], **kwargs):
+        super().__init__(variables, **kwargs)
         if not ("week" in self.var_names or "month" in self.var_names):
             raise ValueError("SeasonalityModelVariables must include 'week' or 'month'.")
         dtime = "week" if "week" in self.var_names else "month"
@@ -49,12 +58,12 @@ class SeasonalityModelVariables(ModelVariables):
             for i in range(1)
         ])
         self.dtime_var.add_priors(regmod.prior.LinearUniformPrior(mat=mat, ub=0.0, lb=0.0))
-        return regmod.model.PoissonModel(data, self.variables, use_offset=True)
+        return super().get_model(data)
 
 
 class TimeModelVariables(ModelVariables):
-    def __init__(self, variables: List[regmod.variable.Variable]):
-        super().__init__(variables)
+    def __init__(self, variables: List[regmod.variable.Variable], **kwargs):
+        super().__init__(variables, **kwargs)
         if "time" not in self.var_names:
             raise ValueError("TimeModelVariables must include 'time'.")
         self.time_var = self.var_dict["time"]
