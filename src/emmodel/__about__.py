c49cac0a304afaa907e2cf4076bc9ba1ce863fb2
zhengp0 <zhengp@uw.edu>
2021-07-08 14:08:28 -0700

    advance to version 0.0.2

--- src/emmodel/__about__.py
+++ src/emmodel/__about__.py
@@ -7,10 +7,10 @@ __title__ = "emmodel"
 __summary__ = "Excess mortality model"
 __uri__ = "https://github.com/ihmeuw-msca/emmodel"
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __author__ = "IHME Math Sciences"
 __email__ = "ihme.math.sciences@gmail.com"
 
 __license__ = "BSD-2-Clause License"
-__copyright__ = f"Copyright 2020 {__author__}"
+__copyright__ = f"Copyright 2021 {__author__}"
